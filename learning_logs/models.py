121347b0c5dc47fb8f8761dad3081b52f6b1228f
Lisl <191439423@qq.com>
2018-08-10 20:39:12 +0800

    add project learning_log

---
+++ learning_logs/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Topic(models.Model):
+	"""docstring for Topic"""
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+	owner = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.text
+
+
+
+class  Entry(models.Model):
+	"""docstring for  Entry"""
+	topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		"""docstring for Meta"""
+		verbose_name_plural = 'entries'
+
+
+	def __str__(self):
+		return self.text[:50] + "..."
+		
